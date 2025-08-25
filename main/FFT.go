7bc787d3eca8cf23f7aa7482c302616321219ef8
Prayush Giri <prayushgiri@Prayushs-MacBook-Pro.local>
2025-08-25 22:14:55 +0530

    implemented fft, magnitudes, peaks

--- main/FFT.go
+++ main/FFT.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
+
 	"github.com/mjibson/go-dsp/fft"
 )
 
 func convertToFloat64(data []int16) []float64 {
-	dataFloat64 := []float64{}
+	dataFloat64 := make([]float64, len(data))
 	for i := 0; i < len(data); i++ {
 		dataFloat64[i] = float64(data[i])
 	}
@@ -26,6 +28,42 @@ func applyFFT(chunk []float64) []complex128 {
 	return fft.FFTReal(chunk)
 }
 
+func extractMagnitudes(fftData []complex128) []float64 {
+	magnitudes := make([]float64, len(fftData))
+	for i := 0; i < len(fftData); i++ {
+		magnitudes[i] = cmplx.Abs(fftData[i])
+	}
+	return magnitudes
+}
+
+type Peak struct {
+	Frequency float64
+	Magnitude float64
+	TimeChunk int
+}
+
+func findPeaks(magnitudes []float64, threshold float64, binWidth float64, chunkIndex int) []Peak {
+	peaks := []Peak{}
+
+	for i := 1; i < len(magnitudes)-1; i++ {
+		if magnitudes[i] > magnitudes[i-1] &&
+			magnitudes[i] > magnitudes[i+1] &&
+			magnitudes[i] > threshold {
+
+			peak := Peak{
+				Frequency: float64(i) * binWidth,
+				Magnitude: magnitudes[i],
+				TimeChunk: chunkIndex,
+			}
+			peaks = append(peaks, peak)
+		}
+	}
+
+	return peaks
+}
+
+
+// what should be the return type of this function
 func FFT(allAudioData []int16) {
 	totalChunk := 256
 
@@ -36,13 +74,27 @@ func FFT(allAudioData []int16) {
 		//convert each of []int64 to []float64 for FFT conversion.
 		chunkFloat := convertToFloat64(chunk)
 
-		//apply fft function for each chunk	
-		
+		//apply fft function for each chunk
+		processed_Data := applyFFT(chunkFloat)
+
+		//extract magnitutde:
+		magnitudes := extractMagnitudes(processed_Data)
+		fmt.Println("first five magnitues: ", magnitudes[:5])
+
+		//find peaks
+		peaks := findPeaks(magnitudes, 50000.0, 44100/1024, i)
+
+
+		if i%50 == 0 && len(peaks) > 0 {
+			fmt.Printf("Chunk %d peaks at frequencies: ", i)
+			for _, peak := range peaks[:5] {
+				fmt.Printf("Frequency: %f, Magnitude: %f\n", peak.Frequency, peak.Magnitude)
+			}
+		}
 		/*
 			TODO:
 					1. Apply FFT
 						* Converting Time Domain data into frequency domain data
-
 					2. Extract magnitude
 					3. find peaks
 		*/
